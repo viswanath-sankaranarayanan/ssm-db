776c7fdde7cfeaae16913332fb5dd484bbf2aa19
Viswanath Sankaranarayanan <viswanathkrishnan.sankaran@gmail.com>
2024-07-11 16:14:29 +0530

    Initial commit

---
+++ DDL/sms_Queries.sql
@@ -0,0 +1,11 @@
+-- DROP SCHEMA ssm;
+
+CREATE SCHEMA IF NOT EXISTS ssm AUTHORIZATION postgres;
+
+--CREATE TABLE IF NOT EXISTSs
+-- ssm.code_master definition
+
+-- Drop table
+
+-- DROP TABLE ssm.code_master;
+
